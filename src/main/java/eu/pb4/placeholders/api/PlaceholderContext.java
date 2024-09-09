525fce4c289e575c6dbb4cd70748ff5041a16384
unilock <unilock@fennet.rentals>
2024-09-09 16:47:18 -0400

    PlaceholderContext: fix usage of Entity instead of ICommandSender + other fixes
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/eu/pb4/placeholders/api/PlaceholderContext.java
+++ src/main/java/eu/pb4/placeholders/api/PlaceholderContext.java
@@ -2,16 +2,18 @@ package eu.pb4.placeholders.api;
 
 import com.github.bsideup.jabel.Desugar;
 import com.mojang.authlib.GameProfile;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 import javax.annotation.Nullable;
 
 @Desugar
 public record PlaceholderContext(MinecraftServer server,
-                                 Entity source,
+                                 ICommandSender source,
                                  @Nullable WorldServer world,
                                  @Nullable EntityPlayerMP player,
                                  @Nullable Entity entity,
@@ -43,23 +45,52 @@ public record PlaceholderContext(MinecraftServer server,
     }
 
     public static PlaceholderContext of(MinecraftServer server) {
-        return new PlaceholderContext(server,  server.getCommandSenderEntity(), null, null, null, null);
+        return new PlaceholderContext(server,  server, null, null, null, null);
     }
 
-    // TODO: not sure about this one
     public static PlaceholderContext of(GameProfile profile, MinecraftServer server) {
-        return new PlaceholderContext(server, server.getCommandSenderEntity(), null, null, null, profile);
+        var name = profile.getName() != null ? profile.getName() : profile.getId().toString();
+        return new PlaceholderContext(server, new ICommandSender() {
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public boolean canUseCommand(int permLevel, String commandName) {
+                return false;
+            }
+
+            @Override
+            public World getEntityWorld() {
+                return server.getWorld(0);
+            }
+
+            @Nullable
+            @Override
+            public MinecraftServer getServer() {
+                return server;
+            }
+        }, null, null, null, profile);
     }
 
     public static PlaceholderContext of(EntityPlayerMP player) {
-        return new PlaceholderContext(player.getServer(), player.getCommandSenderEntity(), player.getServerWorld(), player, player, player.getGameProfile());
+        return new PlaceholderContext(player.getServer(), player, player.getServerWorld(), player, player, player.getGameProfile());
+    }
+
+    public static PlaceholderContext of(ICommandSender source) {
+        if (source.getCommandSenderEntity() instanceof EntityPlayerMP player) {
+            return of(player);
+        } else {
+            return new PlaceholderContext(source.getServer(), source, (WorldServer) source.getEntityWorld(), null, source.getCommandSenderEntity(), null);
+        }
     }
 
     public static PlaceholderContext of(Entity entity) {
         if (entity instanceof EntityPlayerMP player) {
             return of(player);
         } else {
-            return new PlaceholderContext(entity.getServer(), entity.getCommandSenderEntity(), (WorldServer) entity.getEntityWorld(), null, entity, null);
+            return new PlaceholderContext(entity.getServer(), entity, (WorldServer) entity.getEntityWorld(), null, entity, null);
         }
     }
 }
